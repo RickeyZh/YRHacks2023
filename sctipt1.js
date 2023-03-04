00b7029e263932731fd5d23dc75dca7f9e4702aa
Eric Chen <70295495+cloudec11@users.noreply.github.com>
2023-03-04 09:00:24 -0500

    image perspective rotate

--- sctipt1.js
+++ sctipt1.js
@@ -1,16 +1,50 @@
 const follow = document.getElementById("cursor-follow");
+const limit = 20;
+const mouseOver = document.getElementById("stare");
+const imgLayer = document.getElementById("st-con");
 
 document.body.onpointermove = event =>{
     
 
         const {pageX, pageY} = event;
+        var {clientX, clientY} = event;
         
         follow.animate({
             left: `${pageX}px`,
             top: `${pageY}px`
         },{
             duration:2200, fill:"forwards"
-        })
+        });
 
+        // let xy = [clientX, clientY];
+        // let pos = xy.concat([imgLayer]);
+    
+        // window.requestAnimationFrame(function(){
+        //     transformLayer(imgLayer, pos);
+        // });
 
 }
+
+
+
+
+function transformimg(x, y, di){
+    let image = di.getBoundingClientRect();
+
+    let deltaX = -(y - image.y - (image.height/2))/limit;
+    let deltaY = (x-image.x - (image.width/2)) / limit;
+
+    return " rotateX("+deltaX+"deg)"
+        +" rotateY("+deltaY+"deg)";
+};
+
+
+function transformLayer(di, xyDI){
+    console.log("gelo");
+    di.style.transform = transformimg.apply(null, xyDI);
+}
+
+mouseOver.onmousemove = function(e){
+
+
+};
