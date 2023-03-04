3b3439174fd932e53317b821cf9a8b4e60905891
Eric Chen <70295495+cloudec11@users.noreply.github.com>
2023-03-04 10:03:57 -0500

    Update sctipt1.js

--- sctipt1.js
+++ sctipt1.js
@@ -15,36 +15,4 @@ document.body.onpointermove = event =>{
         },{
             duration:2200, fill:"forwards"
         });
-
-        // let xy = [clientX, clientY];
-        // let pos = xy.concat([imgLayer]);
-    
-        // window.requestAnimationFrame(function(){
-        //     transformLayer(imgLayer, pos);
-        // });
-
 }
-
-
-
-
-function transformimg(x, y, di){
-    let image = di.getBoundingClientRect();
-
-    let deltaX = -(y - image.y - (image.height/2))/limit;
-    let deltaY = (x-image.x - (image.width/2)) / limit;
-
-    return " rotateX("+deltaX+"deg)"
-        +" rotateY("+deltaY+"deg)";
-};
-
-
-function transformLayer(di, xyDI){
-    console.log("gelo");
-    di.style.transform = transformimg.apply(null, xyDI);
-}
-
-mouseOver.onmousemove = function(e){
-
-
-};
