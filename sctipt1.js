e71f12f2baba5e1cc89fc6395a0088cf53ddc98e
Eric Chen <70295495+cloudec11@users.noreply.github.com>
2023-03-03 22:29:32 -0500

    created infromation section and fixed scrolling

--- sctipt1.js
+++ sctipt1.js
@@ -1,13 +1,16 @@
 const follow = document.getElementById("cursor-follow");
 
 document.body.onpointermove = event =>{
-    const {clientX, clientY} = event;
+    
 
-    follow.animate({
-        left: `${clientX}px`,
-        top: `${clientY}px`
-    },{
-        duration:2200, fill:"forwards"
-    })
+        const {clientX, clientY} = event;
+        
+        follow.animate({
+            left: `${clientX}px`,
+            top: `${clientY}px`
+        },{
+            duration:2200, fill:"forwards"
+        })
+
 
 }
