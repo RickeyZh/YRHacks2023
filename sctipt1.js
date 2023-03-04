ad8450ce09b6b69d49928f38b9f9e1553e5fb02a
Eric Chen <70295495+cloudec11@users.noreply.github.com>
2023-03-04 15:55:46 -0500

    hamburger on cam page

--- sctipt1.js
+++ sctipt1.js
@@ -21,8 +21,6 @@ const nav = document.getElementById('menu')
 
 btn.addEventListener('click', ()=>{
     btn.classList.toggle('open')
-    // nav.classList.toggle('flex')
-    // nav.classList.toggle('hidden')
 })
 
 btn.addEventListener('click', ()=>{
