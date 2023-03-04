03da32cbf2d09f0aecc46cc85ffb2c9a41e316f5
Eric Chen <70295495+cloudec11@users.noreply.github.com>
2023-03-04 15:17:26 -0500

    hamburger menu

--- sctipt1.js
+++ sctipt1.js
@@ -16,3 +16,22 @@ document.body.onpointermove = event =>{
             duration:2200, fill:"forwards"
         });
 }
+const btn = document.getElementById('menu-btn')
+const nav = document.getElementById('menu')
+
+btn.addEventListener('click', ()=>{
+    btn.classList.toggle('open')
+    // nav.classList.toggle('flex')
+    // nav.classList.toggle('hidden')
+})
+
+btn.addEventListener('click', ()=>{
+    if(!nav.classList.contains('menu-active')){
+        nav.classList.remove('menu-inactive')
+        nav.classList.add('menu-active')
+        nav.classList.add('d-flex')
+    } else {
+        nav.classList.remove('menu-active')
+        nav.classList.add('menu-inactive')
+    }
+})
