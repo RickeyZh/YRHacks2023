0360fcfd71155f4a03ba9fba679d83ff0804da65
RickeyZh <rickey.zhengg@gmail.com>
2023-03-03 23:12:31 -0500

    Working test

---
+++ posenet.js
@@ -0,0 +1,41 @@
+let video;
+let poseNet;
+let poses = [];
+
+function setup() {
+    const canvas = createCanvas(640, 480);
+    canvas.parent('videoContainer');
+
+    // Video capture
+    video = createCapture(VIDEO);
+    video.size(width, height);
+    // Create a new poseNet method with a single detection
+    poseNet = ml5.poseNet (video, modelReady);
+    // This sets up an event that fills the global variable "poses"
+    // with an array every time new poses are detected
+    poseNet.on('pose', function(results) {
+        poses = results;
+    });
+    console.log(poses)
+    //video.hide();
+}
+
+function draw() {
+    image (video, 0, 0, width, height);
+    // We can call both functions to draw all keypoints and the skeletons
+    drawKeypoints();
+}
+function modelReady(){
+    select('#status').html('model Loaded')
+}
+function drawKeypoints(){
+    for(let i=0; i<poses.length; i++){
+        let pose = poses[i].pose;
+        for(let j=0; j<pose.keypoints.length; j++){
+            let keypoint = pose.keypoints[j];
+            fill(255, 0, 0);
+            noStroke();
+            ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
+        }
+    }
+}
